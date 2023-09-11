0178effb2977a1bb4c7b1c9d37635c687ad45370
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-11 13:20:33 +0600

    Delete api has been created

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dracezw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,22 +29,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     //mongo db data base collection function
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
-
+    //coffee get api
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-
-
     });
+    
+    //coffee add data api
     app.post('/coffee', async (req, res) => {
       const result = await coffeeCollection.insertOne(req.body);
       res.send(result);
     });
 
+    //coffee delete data api
+    app.delete('/coffee/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
